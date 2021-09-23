4093195c5b0f3f416135a324f05dd4c471931f0e
omar11011 <atarama11011@gmail.com>
2021-09-23 03:18:30 -0500

    Sorting folders

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const path = require('path')
 const express = require('express')
-const router = require('./backend/router/')
+const router = require('./router/')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.set('views', path.join(__dirname, 'frontend/views'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(router)
 
-app.use(express.static(path.join(__dirname, 'frontend/public/')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(port, () => {
     console.log('Server on port', port)
